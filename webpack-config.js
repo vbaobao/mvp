3d2c51b39471f8b20b1101ff5dac6172196e2cdf
vbaobao <vbao.main@gmail.com>
2021-02-02 12:37:22 -0800

    Set up webpack config

---
+++ webpack-config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './client'),
+    filename: 'bundle.[contenthash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+}
