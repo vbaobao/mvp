364217b6c347c662b11abbdb6ff1ecdafedb1e79
vbaobao <vbao.main@gmail.com>
2021-02-02 17:53:49 -0800

    Add skeleton React app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Dashboard from 'dashboard.jsx';
+import NewClient from 'form_newclient.jsx';
+import NewShipment from 'form_newshipment.jsx';
 
 function App(props) {
   return (<h1>HELLO</h1>);
---
+++ src/dashboard.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+class Dashboard {}
+
+export default Dashboard;
