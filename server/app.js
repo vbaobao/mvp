6464ae8a83526e9db34aa13d2fa7d6486c7d5d22
vbaobao <vbao.main@gmail.com>
2021-02-03 23:08:57 -0800

    Write base routed

--- server/app.js
+++ server/app.js
@@ -11,3 +11,15 @@ app.listen(port, ()=> {
 app.get('/', (req, res) => {
   res.send('Hello World?');
 });
+
+app.post('/newclient', (req,res) => {
+  res.send('New client!');
+});
+
+app.post('/newshipment', (req,res) => {
+  res.send('New shipment!');
+});
+
+app.post('/complete', (req,res) => {
+  res.send('Shipment completed!');
+});
