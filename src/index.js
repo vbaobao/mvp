ea16d399476dc7c62a848276dca12424e3d4f64e
vbaobao <vbao.main@gmail.com>
2021-02-03 18:08:01 -0800

    Change file names to follow component convention

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App.jsx';
 
-render(<h1>HI</h1>, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
