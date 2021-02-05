d4ce61ff0ea17701e9d1ed525ce24cd9be1f591f
vbaobao <vbao.main@gmail.com>
2021-02-04 23:56:57 -0800

    Functional new client post request handler

--- src/ui/NewClient.jsx
+++ src/ui/NewClient.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 
 function NewClient (props) {
   function submitClient(e) {
     e.preventDefault();
-    console.log('New client submitted');
+    let newClientData = {};
+    for (const input of e.target) {
+      newClientData[input.name] = input.value;
+    }
+    axios.post('/newclient', newClientData)
+      .then(() => alert(`Your new client ${newClientData.firstname} ${newClientData.lastname} has been added.`))
+      .catch((err) => console.error(err.message));
   }
   return (
     <div>
@@ -25,7 +32,7 @@ function NewClient (props) {
           Email Address:
           <input type='email' name='email' />
         </label>
-        <input type='submit' value='Add new client' />
+        <input type='submit' name='submit' value='Add new client' />
       </form>
     </div>
   );
