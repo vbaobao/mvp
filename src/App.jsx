d9f389724807253f5e8fe887a12124361daa6297
vbaobao <vbao.main@gmail.com>
2021-02-05 14:42:29 -0800

    Added state and state values to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,34 @@ import Forms from './ui/Forms.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      clientdata: null,
+      shipmentdata: null
+    };
   }
 
   componentDidMount() {
     // Use router to GET data to add to state
     axios.get('/shipmentdata')
-      .then(res => console.log('Shipments: ', res.data))
+      .then(res => {
+        console.log('Shipments: ', res.data);
+        this.setState({shipmentdata: res.data});
+      })
       .catch((err) => console.error(err.message));
 
     axios.get('/clientdata')
-      .then((res) => console.log('Clients: ', res.data))
+      .then((res) => {
+        console.log('Clients: ', res.data);
+        this.setState({clientdata: res.data});
+      })
       .catch((err) => console.error(err.message));
   }
 
   render () {
     return (
       <div>
-        <Dashboard />
-        <Forms />
+        <Dashboard shipmentdata={this.state.clientdata} />
+        <Forms clientdata={this.state.clientdata}/>
       </div>
       );
   };
