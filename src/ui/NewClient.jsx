c9d176673f28722f87d3d7c42398f8105da706b2
vbaobao <vbao.main@gmail.com>
2021-02-04 23:40:01 -0800

    Added minor CSS styling for readability

--- src/ui/NewClient.jsx
+++ src/ui/NewClient.jsx
@@ -6,25 +6,28 @@ function NewClient (props) {
     console.log('New client submitted');
   }
   return (
-    <form className='newclientform' onSubmit={submitClient}>
-      <label name='firstname'>
-        First Name:
-        <input type='text' name='firstname' required />
-      </label>
-      <label name='lastname'>
-        Last Name:
-        <input type='text' name='lastname' required />
-      </label>
-      <label name='phone'>
-        Phone Number:
-        <input type='text' name='phone' />
-      </label>
-      <label name='email'>
-        Email Address:
-        <input type='email' name='email' />
-      </label>
-      <input type='submit' value='Add new client' />
-    </form>
+    <div>
+      <h2>Add a new client:</h2>
+      <form className='newclientform' onSubmit={submitClient}>
+        <label name='firstname'>
+          First Name:
+          <input type='text' name='firstname' required />
+        </label>
+        <label name='lastname'>
+          Last Name:
+          <input type='text' name='lastname' required />
+        </label>
+        <label name='phone'>
+          Phone Number:
+          <input type='text' name='phone' />
+        </label>
+        <label name='email'>
+          Email Address:
+          <input type='email' name='email' />
+        </label>
+        <input type='submit' value='Add new client' />
+      </form>
+    </div>
   );
 }
 
--- src/ui/NewShipment.jsx
+++ src/ui/NewShipment.jsx
@@ -10,32 +10,35 @@ function NewShipment(props) {
   }
 
   return (
-    <form className='newshipmentform' onSubmit={submitShipment}>
-      <label name='clientname'>
-        Client:
-        <select name='clientname' required>
-          <option value=''></option>
-          <option value='test'>TEST</option>
-        </select>
-      </label>
-      <label name='volume'>
-        Order Volume:
-        <input type='number' name='volume' required />
-      </label>
-      <label name='charge'>
-        Charge:
-        <input type='number' name='charge' required />
-      </label>
-      <label name='cost'>
-        Cost:
-        <input type='number' name='cost' required />
-      </label>
-      <label name='address'>
-        Address:
-        <input type='text' name='address' required />
-      </label>
-      <input type='submit' value='Add new shipment' />
-    </form>
+    <div>
+      <h2>Add a new shipment:</h2>
+      <form className='newshipmentform' onSubmit={submitShipment}>
+        <label name='clientname'>
+          Client:
+          <select name='clientname' required>
+            <option value=''></option>
+            <option value='test'>TEST</option>
+          </select>
+        </label>
+        <label name='volume'>
+          Order Volume:
+          <input type='number' name='volume' required />
+        </label>
+        <label name='charge'>
+          Charge:
+          <input type='number' name='charge' required />
+        </label>
+        <label name='cost'>
+          Cost:
+          <input type='number' name='cost' required />
+        </label>
+        <label name='address'>
+          Address:
+          <input type='text' name='address' required />
+        </label>
+        <input type='submit' value='Add new shipment' />
+      </form>
+    </div>
   );
 }
 
