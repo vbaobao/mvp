57083b42f05223a6132cf6dd441c97469320303c
vbaobao <vbao.main@gmail.com>
2021-02-05 21:52:00 -0800

    Functional completed shipment dashboard and DB update

--- src/ui/CompletedShipments.jsx
+++ src/ui/CompletedShipments.jsx
@@ -1,12 +1,48 @@
 import React from 'react';
+import axios from 'axios';
 
 function CompletedShipments(props) {
-  console.log('Completed shipments: ', props.shipments);
+
+  function updateStatus(e, shipmentID) {
+    axios.post('/complete', {id: shipmentID, is_complete: 0})
+      .then(() => console.log(`Shipment ${shipmentID}'s status has been updated`))
+      .catch((err) => console.error(err.message));
+  }
+
+  let table = props.shipments.map((shipment) => {
+    return (
+      <tr key={shipment.id}>
+        <td>{shipment.id}</td>
+        <td>{shipment.firstname} {shipment.lastname}</td>
+        <td>{shipment.email}</td>
+        <td>{shipment.volume}</td>
+        <td>{shipment.profit}</td>
+        <td>
+          <button type='button' onClick={(e) => {updateStatus(e, shipment.id);}}>Mark active</button>
+        </td>
+      </tr>
+    );
+  });
+
   // For each to create table rows and data
   return (
     <div>
       <h2>Shipment History</h2>
-      <table></table>
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>Client</th>
+            <th>Email</th>
+            <th>Volume</th>
+            <th>Profit</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {table}
+        </tbody>
+      </table>
     </div>
   );
 }
