34dac6e61c81de5bc5cd44cfde4316c62f59336f
vbaobao <vbao.main@gmail.com>
2021-02-06 10:45:05 -0800

    Attempt at clearing form after submit

--- src/ui/NewClient.jsx
+++ src/ui/NewClient.jsx
@@ -1,32 +1,63 @@
 import React from 'react';
 import axios from 'axios';
 
-function NewClient (props) {
+class NewClient extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div>
-      <h2>Add a new client:</h2>
-      <form className='newclientform' onSubmit={props.submitClient}>
-        <label name='firstname'>
-          First Name:
-          <input type='text' name='firstname' required />
-        </label>
-        <label name='lastname'>
-          Last Name:
-          <input type='text' name='lastname' required />
-        </label>
-        <label name='phone'>
-          Phone Number:
-          <input type='text' name='phone' />
-        </label>
-        <label name='email'>
-          Email Address:
-          <input type='email' name='email' />
-        </label>
-        <input type='submit' name='submit' value='Add new client' />
-      </form>
-    </div>
-  );
+    this.handleChange = this.handleChange.bind(this);
+    this.resetForm = this.resetForm.bind(this);
+
+    this.state = {
+      firstname: '',
+      lastname: '',
+      phone: '',
+      email: ''
+    }
+  }
+
+  handleChange(e) {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  resetForm() {
+    //Seems to override the submit form function
+    this.setState(
+      {
+        firstname: '',
+        lastname: '',
+        phone: '',
+        email: ''
+      }
+    );
+  }
+
+  render () {
+    return (
+      <div>
+        <h2>Add a new client:</h2>
+        <form className='newclientform' onSubmit={this.props.submitClient}>
+          <label name='firstname'>
+            First Name:
+            <input type='text' name='firstname' value={this.state.firstname} onChange={this.handleChange} required />
+          </label>
+          <label name='lastname'>
+            Last Name:
+            <input type='text' name='lastname' value={this.state.lastname} onChange={this.handleChange} required />
+          </label>
+          <label name='phone'>
+            Phone Number:
+            <input type='text' name='phone' value={this.state.phone} onChange={this.handleChange} />
+          </label>
+          <label name='email'>
+            Email Address:
+            <input type='email' name='email' value={this.state.email} onChange={this.handleChange} />
+          </label>
+          <input type='submit' name='submit' value='Add new client' />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default NewClient;
